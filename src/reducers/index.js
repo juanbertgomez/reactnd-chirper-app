4d24cf29cbfa9582c17bbc1a9498d1881d1c4b39
Juan Gomez <juanbertgomez@gmail.com>
2018-07-19 10:51:00 +0200

    feature: add dashboard component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
-import { combineReducers} from 'redux'
-import { authedUser } from './authedUser'
-import { tweets } from './tweets'
-import {users } from './users'
+import { combineReducers } from 'redux'
+import  authedUser from './authedUser'
+import  tweets from './tweets'
+import users from './users'
 
 export default combineReducers({
     authedUser,
