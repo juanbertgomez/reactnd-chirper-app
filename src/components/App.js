5f36adabf890fdd502fe4e30cc81bddcf7daca8a
Juan Gomez <juanbertgomez@gmail.com>
2018-07-19 12:22:15 +0200

    feature: add tweet component a loading bar
    * Tweet component: has all the elements and the logic to display those elements
    * Loading Bar: import react library, add it as a reducer and component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
   componentDidMount() {
@@ -10,7 +11,13 @@ class App extends Component {
 
   render() {
     return (
-      <Dashboard/>
+      <div> 
+        <LoadingBar />
+        {this.props.loading === true
+          ? null
+          : <Dashboard/>
+        }
+      </div>
     )
   }
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Tweet from './Tweet'
 
 class Dashboard extends Component {
     render () {
-        console.log(this.props)
         return (
             <div>
                 <h3 className='center'> Your Timeline</h3>
@@ -11,7 +11,7 @@ class Dashboard extends Component {
                     {this.props.tweetsIds.map((id) =>
                     (
                         <li key={id}>
-                            <div>TWEET ID: { id }</div>
+                            <Tweet id={ id }/>
                         </li>
                     ))}
                 </ul>
